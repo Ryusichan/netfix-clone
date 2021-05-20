3b3405b46e86c5d9c5344520567e88e4448fba3f
Ryusichan <Ryusichan@github.com>
2021-05-20 17:05:56 +0900

    scroll

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
+import styled from 'styled-components'
+
+const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
@@ -7,18 +10,55 @@ function Row({title, fetchUrl }) {
     useEffect(() => {
         async function fetchData(){
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results);
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+
+    console.log(movies);
+
+    const Rowposters = styled.div`
+        display: flex;
+        overflow-y: hidden;
+        overflow-x: scroll;
+        padding: 20px;
+
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    `
+        
+    const Rowposter = styled.img`
+        object-fit: contain;
+        width: 100%;
+        max-height: 100px;
+        margin-right: 10px;
+        transition: transform 450ms;
+        
+        &:hover {
+            transform: scale(1.08);
+        }
+    `
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
-
+            <Rowposters>
+                {/* {posters} */}
+                {movies.map(movie => (
+                    <Rowposter
+                        key={movie.id}
+                        className="row_poster"
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name}
+                    />
+                ))}
+            </Rowposters>
         </div>
     )
+    
+
 }
 
 export default Row;
